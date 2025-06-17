13b835103f5ebd6cd015ba1ab4276729a20cc539
Артём Кравцов <a.kravtsov@globexit.ru>
2025-06-17 23:57:21 +0300

    feat: Добавлен checkbox с фильтром

--- src/store/taskSlice.ts
+++ src/store/taskSlice.ts
@@ -3,62 +3,68 @@ import type { PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "./configureStore";
 
 export interface taskListState {
-  list: Task[];
-  notification: string;
+	list: Task[];
+	isShowClosedTask: boolean;
+	notification: string;
 }
 
 const initialState: taskListState = {
-  list: [],
-  notification: "",
+	list: [],
+	isShowClosedTask: true,
+	notification: "",
 };
 
 export const taskListSlice = createSlice({
-  name: "taskList",
-  initialState,
-  reducers: {
-    addTask: (state, action: PayloadAction<Task["header"]>) => {
-      state.list.push({
-        id: crypto.randomUUID(),
-        header: action.payload,
-        done: false,
-      });
-    },
-    completeTask: (state, action: PayloadAction<Task["id"]>) => {
-      const task = state.list.find((x) => x.id === action.payload);
+	name: "taskList",
+	initialState,
+	reducers: {
+		addTask: (state, action: PayloadAction<Task["header"]>) => {
+			state.list.push({
+				id: crypto.randomUUID(),
+				header: action.payload,
+				done: false,
+			});
+		},
+		completeTask: (state, action: PayloadAction<Task["id"]>) => {
+			const task = state.list.find((x) => x.id === action.payload);
 
-      if (task) {
-        task.done = true;
-      }
-    },
-    toggleTask: (state, action: PayloadAction<Task["id"]>) => {
-      const task = state.list.find((x) => x.id === action.payload);
+			if (task) {
+				task.done = true;
+			}
+		},
+		toggleTask: (state, action: PayloadAction<Task["id"]>) => {
+			const task = state.list.find((x) => x.id === action.payload);
 
-      if (task) {
-        task.done = !task.done;
+			if (task) {
+				task.done = !task.done;
 
-        if (task.done) {
-          state.notification = `Задача "${task.header}" завершена`;
-        }
-      }
-    },
-    deleteTask: (state, action: PayloadAction<Task["id"]>) => {
-      state.list = state.list.filter((x) => x.id !== action.payload);
-    },
-    setNotification: (state, action: PayloadAction<Task["header"]>) => {
-      state.notification = `Задача "${action.payload}" завершена`;
-    },
-    clearNotification: (state) => {
-      state.notification = "";
-    },
-  },
+				if (task.done) {
+					state.notification = `Задача "${task.header}" завершена`;
+				}
+			}
+		},
+		deleteTask: (state, action: PayloadAction<Task["id"]>) => {
+			state.list = state.list.filter((x) => x.id !== action.payload);
+		},
+		setNotification: (state, action: PayloadAction<Task["header"]>) => {
+			state.notification = `Задача "${action.payload}" завершена`;
+		},
+		clearNotification: (state) => {
+			state.notification = "";
+		},
+		toggleFilterClosedTask(state) {
+			state.isShowClosedTask = !state.isShowClosedTask
+		}
+	},
 });
 
 export const {
-  addTask,
-  completeTask,
-  deleteTask,
-  toggleTask,
-  clearNotification,
+	addTask,
+	completeTask,
+	deleteTask,
+	toggleTask,
+	clearNotification,
+	toggleFilterClosedTask
 } = taskListSlice.actions;
 
 export default taskListSlice.reducer;
@@ -68,10 +74,13 @@ export const tasksSelector = (state: RootState) => state.taskList.list;
 export const fullCount = (state: RootState) => state.taskList.list.length;
 
 export const completeCount = (state: RootState) =>
-  state.taskList.list.filter((x) => x.done).length;
+	state.taskList.list.filter((x) => x.done).length;
 
 export const uncompleteCount = (state: RootState) =>
-  state.taskList.list.filter((x) => !x.done).length;
+	state.taskList.list.filter((x) => !x.done).length;
 
 export const getNotification = (state: RootState) =>
-  state.taskList.notification
+	state.taskList.notification
+
+export const isShowClosedTask = (state: RootState) =>
+	state.taskList.isShowClosedTask
