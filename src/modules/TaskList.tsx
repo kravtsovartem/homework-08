8e0048226cbec2bb87cf4635e6b0d197383ccc38
Артём Кравцов <a.kravtsov@globexit.ru>
2025-06-18 00:06:15 +0300

    feat(TaskList.tsx): Добавлена фильтрация задач для списка

--- src/modules/TaskList.tsx
+++ src/modules/TaskList.tsx
@@ -1,11 +1,23 @@
+import { useCallback, useMemo } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Empty } from "src/components/Empty";
 import { List } from "src/components/List";
-import { deleteTask, tasksSelector, toggleTask } from "src/store/taskSlice";
+import {
+  deleteTask,
+  isShowClosedTask,
+  tasksSelector,
+  toggleTask,
+} from "src/store/taskSlice";
 
 export const TaskList = () => {
   const items = useSelector(tasksSelector);
   const dispatch = useDispatch();
+
+  const showClosedTask = useSelector(isShowClosedTask);
+
+  const computedList = useMemo(() => {
+    return showClosedTask ? items.filter((item) => item.done) : items;
+  }, [showClosedTask]);
 
   const handleDelete = (id: Task["id"]) => {
     dispatch(deleteTask(id));
@@ -15,8 +27,12 @@ export const TaskList = () => {
     dispatch(toggleTask(id));
   };
 
-  return items.length > 0 ? (
-    <List items={items} onDelete={handleDelete} onToggle={handleToggle} />
+  return computedList.length > 0 ? (
+    <List
+      items={computedList}
+      onDelete={handleDelete}
+      onToggle={handleToggle}
+    />
   ) : (
     <Empty />
   );
